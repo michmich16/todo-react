d24f938c97113458286e91ab8462d217a53dcbb0
Michael P <102651473+michmich16@users.noreply.github.com>
2024-08-22 14:38:08 +0200

    er igang med modal but damn

---
+++ todo-project/src/components/Modal/Modal.jsx
@@ -0,0 +1,11 @@
+import style from './Modal.module.scss';
+import { useState } from 'react';
+
+export const Modal = () =>{
+    return(
+            <div>
+                <h3>Add to your to-do list</h3>
+            </div>
+
+    )
+}
